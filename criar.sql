12eb281618680cd43a05b1859ed8a562e42f2a5a
up201904675 <up201904675@fe.up.pt>
2021-11-29 16:13:35 +0000

    Resolved conflict

--- criar.sql
+++ criar.sql
@@ -65,8 +65,8 @@ CREATE TABLE auction(
     status auction_status NOT NULL,
     category auction_category NOT NULL,
     seller_id INTEGER REFERENCES users(user_id) NOT NULL,
-    win_bid INTEGER REFERENCES bid(bid_id)
-    auction_image INTEGER REFERENCES image(img_id),
+    win_bid INTEGER REFERENCES bid(bid_id),
+    auction_image INTEGER REFERENCES image(img_id)
 );
 
 
