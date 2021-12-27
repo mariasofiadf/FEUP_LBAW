fa25bf22078031754cbac53122b5ab97f66e52b2
up201907284 <up201907284@fe.up.pt>
2021-12-27 11:27:39 +0000

    added auction_follow model & typos

--- app/Models/AuctionFollow.php
+++ app/Models/AuctionFollow.php
@@ -5,12 +5,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class UserFollow extends Model {
+class AuctionFollow extends Model
+{
     use HasFactory;
 
     public $timestamps = false;
 
     protected $fillable = ["id_followed", "id_follower"];
 
-    protected $table = 'user_follow';
+    protected $table = 'auction_follow';
 }
