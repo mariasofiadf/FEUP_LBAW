48fe9ca0ca4477eb2c3c7a9b89157e06935d8e24
Maria Sofia Figueiredo <up201904675@fe.up.pt>
2022-01-01 21:46:34 +0000

    Added bootstrap

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,26 +1,47 @@
 
-<header class = "navbar-container">
-    {{-- Main Navigation Bar --}} 
-  <nav class="navbar">
-    <!-- LOGO -->
-    <a class="navbar-logo"  href="{{ url('/auctions') }}">Hand Of Midas</a>
+<header >
+  <div class "container">
+    <div class="row">
+      <div class="col-3">
+        <a  class="nav-link" href="{{ url('/auctions') }}">Hand Of Midas</a>
+      </div>
+      <div class="col-6">
+        <ul class="nav justify-content-center">
+          <!-- NAVIGATION MENU -->
+          <li class="nav-item">
+            <a class="nav-link" href="/faq">How it Works</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/auctions">Auctions</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/users">Users</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/about">About Us</a>
+          </li>  
+          <li class="nav-item">
+            <a class="nav-link" href="/contact_us">Contact</a>
+          </li>
+        </ul>
+      </div>
+      <div class="col-3">
+        <ul class="nav nav-pills  justify-content-end">
+      @if (Auth::check())
+        <li class="nav-item">
+          <a class = "nav-link" href="/users/{{ Auth::user()->user_id }}">{{ Auth::user()->name }}</a>
+        </li>
 
-    <!-- NAVIGATION MENU -->
-    <div class="nav-links">
-        <ul class = "menu">
-            <li><a href="/faq">How it Works</a></li>
-            <li><a href="/auctions">Auctions</a></li>
-            
-            <li><a href="/users">Users</a></li>
-            <li><a href="/about">About Us</a></li>  
-            <li><a href="/contact_us">Contact</a></li>
-        </ul> 
+        <li class="nav-item">
+          <button class="nav-link active" class="rounded" href="{{ url('/logout') }}"> Logout </a> 
+        </li>
+        @endif
+      </ul>
+      </div>
     </div>
 
-    @if (Auth::check())
-      <a class = "user_name" href="/users/{{ Auth::user()->user_id }}">{{ Auth::user()->name }}</a>
-      <a class="logout-button" href="{{ url('/logout') }}"> Logout </a> 
-    @endif
 
-  </nav>
+
+
+  <div>
   </header>   
