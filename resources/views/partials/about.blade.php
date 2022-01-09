dee7770615e696b53b74bf7710b1b3e7b03383db
up201907284 <up201907284@fe.up.pt>
2022-01-09 19:00:55 +0000

    Initial about text done??

--- resources/views/partials/about.blade.php
+++ resources/views/partials/about.blade.php
@@ -1,5 +1,10 @@
 <p class="display-2 text-center"> About the website </p>
-<div class = "text-md-start">
+<div class = "text-md-start lh-base">
     <p> This website was created with academic purpouses in mind, by the 3rd group in class 2 of the LBAW course, during the 1st semester of
-        the lective year of 2021/2022. </p>
+        the lective year of 2021/2022. The site's founding members are: </p>
+    <ul>
+        <li>Afonso Duarte de Carvalho Monteiro</li>
+        <li>Ana Rita Antunes Ramada</li>
+        <li>Deborah Marques Lago</li>
+        <li>Maria Sofia Diogo Figueiredo</li>
 </div>
