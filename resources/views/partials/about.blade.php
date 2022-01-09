c258632b6b972d2799b3efa8c4bd62339b0d57e3
up201907284 <up201907284@fe.up.pt>
2022-01-09 19:11:07 +0000

    Separated the about page in 2 major themes

--- resources/views/partials/about.blade.php
+++ resources/views/partials/about.blade.php
@@ -1,10 +1,19 @@
 <p class="display-2 text-center"> About the website </p>
+
+<p class = "display-4"> General Information </p>
 <div class = "text-md-start lh-base">
-    <p> This website was created with academic purpouses in mind, by the 3rd group in class 2 of the LBAW course, during the 1st semester of
+    
+    <p> This website was created with academic purposes in mind, by the 3rd group in class 2 of the LBAW course, during the 1st semester of
         the lective year of 2021/2022. The site's founding members are: </p>
     <ul>
-        <li>Afonso Duarte de Carvalho Monteiro</li>
-        <li>Ana Rita Antunes Ramada</li>
-        <li>Deborah Marques Lago</li>
-        <li>Maria Sofia Diogo Figueiredo</li>
+        <li>Afonso Duarte de Carvalho Monteiro (up201907284)</li>
+        <li>Ana Rita Antunes Ramada (up201904565)</li>
+        <li>Deborah Marques Lago (up201806102)</li>
+        <li>Maria Sofia Diogo Figueiredo (up201904675)</li>
+    </ul>
+    
+    <p> In its conception, the idea was to incorporate what was learned during classes in a concrete project, 
+        following one theme out of a pool of them available. In our group's case, it was decided to create an auction website.</p> 
 </div>
+
+<p class = display-4> Services provided </p>
