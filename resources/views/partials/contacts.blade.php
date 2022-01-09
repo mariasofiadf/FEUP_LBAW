0ea23ea1dbdbd4c911e4e4589b69fb4e62ab13bc
up201907284 <up201907284@fe.up.pt>
2022-01-09 22:21:41 +0000

    experiment

--- resources/views/partials/contacts.blade.php
+++ resources/views/partials/contacts.blade.php
@@ -6,23 +6,23 @@
 
 <div class = "container">
     <div class = "row">
-        <div class = "col">Name</div>
-        <div class = "col">email</div>
+        <div class = "col-md">Name</div>
+        <div class = "col-md">email</div>
     </div>
     <div class = "row">
-        <div class = "col">Afonso Duarte de Carvalho Monteiro</div>
-        <div class = "col">up201907284@up.pt</div>
+        <div class = "col-md">Afonso Duarte de Carvalho Monteiro</div>
+        <div class = "col-md">up201907284@up.pt</div>
     </div>
     <div class = "row">
-        <div class = "col">Ana Rita Antunes Ramada</div>
-        <div class = "col">up201904565@up.pt</div>
+        <div class = "col-md">Ana Rita Antunes Ramada</div>
+        <div class = "col-md">up201904565@up.pt</div>
     </div>
     <div class = "row">
-        <div class = "col">Deborah Marques Lago</div>
-        <div class = "col">up201806102@up.pt</div>
+        <div class = "col-md">Deborah Marques Lago</div>
+        <div class = "col-md">up201806102@up.pt</div>
     </div>
     <div class = "row">
-        <div class = "col">Maria Sofia Diogo Figueiredo</div>
-        <div class = "col">up201904675@up.pt</div>
+        <div class = "col-md">Maria Sofia Diogo Figueiredo</div>
+        <div class = "col-md">up201904675@up.pt</div>
     </div>
 </div>
