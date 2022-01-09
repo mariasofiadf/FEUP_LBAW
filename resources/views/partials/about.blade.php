e0757f78a71cdf69ee1312cd037dbf090d65ff2d
up201907284 <up201907284@fe.up.pt>
2022-01-09 18:52:43 +0000

    more text

--- resources/views/partials/about.blade.php
+++ resources/views/partials/about.blade.php
@@ -1,4 +1,5 @@
 <p class="display-2 text-center"> About the website </p>
-<div class = "text-sm-start">
-    <p> This is just a test </p>
+<div class = "text-md-start">
+    <p> This website was created with academic purpouses in mind, by the 3rd group in class 2 of the LBAW course, during the 1st semester of
+        the lective year of 2021/2022. </p>
 </div>
