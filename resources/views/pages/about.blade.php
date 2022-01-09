dec6a2cea4b4a5a0e68492a468989b385f8591ee
up201907284 <up201907284@fe.up.pt>
2022-01-09 18:23:30 +0000

    testing the creation of the page

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('title', 'About Us')
+
+@section('content')
+
+@include('partials.about')
+
+@endsection
---
+++ resources/views/partials/about.blade.php
@@ -0,0 +1,4 @@
+<p class="display-2 text-center"> About the website </p>
+<div class = "text-sm-start">
+    <p> This is just a test </p>
+</div>
