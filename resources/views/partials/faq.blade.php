f915060ae2afeff06e7a4fab88566c618c96adca
up201907284 <up201907284@fe.up.pt>
2022-01-10 10:37:57 +0000

    added a faq

--- resources/views/partials/faq.blade.php
+++ resources/views/partials/faq.blade.php
@@ -1,1 +1,10 @@
 <p class="display-3 text-center"> FAQ by our users </p>
+
+<div class = "text-md-start lh-base">
+    <dl class = "row">
+        <dt class = "col-sm-3">What kind of products are auctioned?</dt>
+        <dd class = "col-sm-9">
+            <p>Mainly Art pieces, books, jewelry and decorations, although it supports other items.</p>
+        </dd>
+    </dl>
+</div>
