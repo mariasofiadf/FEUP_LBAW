82f81fb57c4d1ec76707dd58aeb784397b98993e
up201907284 <up201907284@fe.up.pt>
2022-01-10 10:48:29 +0000

    adjusted size

--- resources/views/partials/faq.blade.php
+++ resources/views/partials/faq.blade.php
@@ -2,18 +2,18 @@
 
 <div class = "text-md-start lh-base fs-5">
     <dl class = "row">
-        <dt class = "col-sm-3 text-truncate">What kind of products are auctioned?</dt>
+        <dt class = "col-sm-6 text-truncate">What kind of products are auctioned?</dt>
         <dd class = "col-sm-9">
             <p>Mainly Art pieces, books, jewelry and decorations, although it supports other items.</p>
         </dd>
 
-        <dt class = "col-sm-3 text-truncate">Is it possible to bid without registering?</dt>
+        <dt class = "col-sm-6 text-truncate">Is it possible to bid without registering?</dt>
         <dd class = "col-sm-9">No, the user must have an account to be allowed to bid.</dd>
 
-        <dt class = "col-sm-3 text-truncate">Is the site completely functional?</dt>
+        <dt class = "col-sm-6 text-truncate">Is the site completely functional?</dt>
         <dd class = "col-sm-9">No, unfortunately there is no real product being sold and any similarities are just coincidences.</dd>
 
-        <dt class = "col-sm-3 text-truncate">Does the admin have absolute power?</dt>
+        <dt class = "col-sm-6 text-truncate">Does the admin have absolute power?</dt>
         <dd class = "col-sm-9">Yes, so you must be carefull about what you say, for the admin watches all...</dd>
     </dl>
 </div>
