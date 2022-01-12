ce90886f66d726c0e15265e3156aafa4d670ffa4
Maria Sofia Figueiredo <up201904675@fe.up.pt>
2022-01-12 21:34:52 +0000

    fixed date input

--- docker_run.sh
+++ docker_run.sh
@@ -5,6 +5,7 @@ cd /var/www; php artisan config:cache
 
 # Add cron job into cronfile
 #* * * * * cd /var/www && php artisan schedule:run >> /dev/null 2>&1
+echo "* * * * * cd /var/www && php artisan schedule:run >> /dev/null 2>&1" >> cronfile
 
 # Install cron job
 crontab cronfile
