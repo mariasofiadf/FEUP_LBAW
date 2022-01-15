840f4f4d7587c7eaa82caca6301754be7df7cbb1
up201907284 <up201907284@fe.up.pt>
2022-01-15 19:01:17 +0000

    typos2

--- resources/views/partials/reportAuction.blade.php
+++ resources/views/partials/reportAuction.blade.php
@@ -5,14 +5,13 @@
         <form method="POST" action="{{ route('auctions/{id}/report', ['id' => $auction->auction_id]) }}">
 
             {{ csrf_field() }}
-            {{ method_field('PUT') }}
             <div class="mb-3" style>
                 <label for="complaint" class="form-label">Reason for complaint</label>
                 <textarea type="text" name="complaint" class="form-control" id="complaint"></textarea>
             </div>
+            <div class="mb-3" style="margin-top:1rem;">
+                <button type="submit" class="btn btn-primary form-control" >Submit</button>
+            </div>
         </form>
-        <div class="mb-3" style="margin-top:1rem;">
-            <button type="submit" class="btn btn-primary form-control" >Submit</button>
-        </div>
     </div>
 </div>
