06aa8467c939b890b269363ec8c8b6d7aa0c679d
up201907284 <up201907284@fe.up.pt>
2022-01-16 16:33:26 +0000

    attempting again

--- resources/views/partials/auctionComplaints.blade.php
+++ resources/views/partials/auctionComplaints.blade.php
@@ -1,1 +1,12 @@
 <p class="display-3 text-center"> Complaints made </p>
+
+<div class = "text-md-start lh-base fs-5">
+    <dl class = "row">
+    @foreach($auctionReports->chunk(8) as $chunk)
+        <dt class = "col-sm-6 text-truncate">{{$chunk->user_id}} said:</dt>
+        <dd class = "col-sm-9">
+            <p>{{$chunk->description}}</p>
+        </dd>
+    </dl>
+
+</div>
