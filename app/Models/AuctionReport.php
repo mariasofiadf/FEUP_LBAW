ea45aec8de0ffeb13fd9f6552b10ff12495d4f1b
up201907284 <up201907284@fe.up.pt>
2022-01-16 15:59:24 +0000

    try a big solution

--- app/Models/AuctionReport.php
+++ app/Models/AuctionReport.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 
 class AuctionReport extends Model
 {
@@ -13,7 +14,10 @@ class AuctionReport extends Model
 
     protected $table = "auction_report";
 
-    //protected $primaryKey = ['auction_id', 'user_id',];
+    protected $primaryKey = ['auction_id', 'user_id',];
+    public $incrementing = false;
+
+
 
     protected $fillable = [
         'description', 'auction_id', 'user_id', 
@@ -22,5 +26,44 @@ class AuctionReport extends Model
     public function user(){return $this->hasOne('App\Models\User');}
 
     public function auction(){return $this->hasOne('App\Models\Auction');}
+
+    /**
+     * Set the keys for a save update query.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    protected function setKeysForSaveQuery(Builder $query)
+    {
+        $keys = $this->getKeyName();
+        if(!is_array($keys)){
+            return parent::setKeysForSaveQuery($query);
+        }
+
+        foreach($keys as $keyName){
+            $query->where($keyName, '=', $this->getKeyForSaveQuery($keyName));
+        }
+
+        return $query;
+    }
+
+    /**
+     * Get the primary key value for a save query.
+     *
+     * @param mixed $keyName
+     * @return mixed
+     */
+    protected function getKeyForSaveQuery($keyName = null)
+    {
+        if(is_null($keyName)){
+            $keyName = $this->getKeyName();
+        }
+
+        if (isset($this->original[$keyName])) {
+            return $this->original[$keyName];
+        }
+
+        return $this->getAttribute($keyName);
+    }
 
 }
