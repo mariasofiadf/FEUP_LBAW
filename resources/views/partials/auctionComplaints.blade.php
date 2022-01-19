200bad899aa243b3ca4105dd431ac488ec290ab4
up201907284 <up201907284@fe.up.pt>
2022-01-19 11:36:06 +0000

    syntax 8

--- resources/views/partials/auctionComplaints.blade.php
+++ resources/views/partials/auctionComplaints.blade.php
@@ -2,8 +2,7 @@
 
 <div class = "text-md-start lh-base fs-5">
     <dl class = "row">
-    @foreach($auctionReports->chunk(8) as $chunk)
-        @foreach($report in $chunk)
+        @foreach($report in $auctionReports)
             <dt class = "col-sm-6 text-truncate">{{$report->user_id}} said:</dt>
             <dd class = "col-sm-9">
                 <p>{{$report->description}}</p>
