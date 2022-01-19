3028a8eba91075f7c8de620aeaa57e674f504224
up201907284 <up201907284@fe.up.pt>
2022-01-19 10:43:13 +0000

    corrected syntax

--- resources/views/partials/auctionComplaints.blade.php
+++ resources/views/partials/auctionComplaints.blade.php
@@ -3,10 +3,10 @@
 <div class = "text-md-start lh-base fs-5">
     <dl class = "row">
     @foreach($auctionReports->chunk(8) as $chunk)
-        <dt class = "col-sm-6 text-truncate">{{$chunk->user_id}} said:</dt>
-        <dd class = "col-sm-9">
-            <p>{{$chunk->description}}</p>
-        </dd>
+        @foreach($report in $chunk)
+            <dt class = "col-sm-6 text-truncate">{{$chunk->user_id}} said:</dt>
+            <dd class = "col-sm-9">
+                <p>{{$chunk->description}}</p>
+            </dd>
     </dl>
-
 </div>
