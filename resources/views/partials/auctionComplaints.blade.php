a37f1a13cba445cd531b4b5fbc85a78303d12457
up201907284 <up201907284@fe.up.pt>
2022-01-20 17:15:41 +0000

    more cleanup

--- resources/views/partials/auctionComplaints.blade.php
+++ resources/views/partials/auctionComplaints.blade.php
@@ -3,7 +3,7 @@
 <div class = "text-md-start lh-base fs-5">
     <dl class = "row">
         @foreach($auctionReports as $report)
-            <dt class = "col-sm-6 text-truncate">The user {{$report->user->username}} said, in the auction {{$report->auction->title}}:</dt>
+            <dt class = "col-sm-6 text-truncate">The user {{$report->user->username}}, in the auction {{$report->auction->title}}, said:</dt>
             <dd class = "col-sm-9">
                 <p>{{$report->description}}</p>
             </dd>
