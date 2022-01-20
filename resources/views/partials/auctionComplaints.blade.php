208ca09caa7c26a6dd0e77162e2ddb1b5d0d78d8
up201907284 <up201907284@fe.up.pt>
2022-01-20 17:01:31 +0000

    correct stuff

--- resources/views/partials/auctionComplaints.blade.php
+++ resources/views/partials/auctionComplaints.blade.php
@@ -3,7 +3,7 @@
 <div class = "text-md-start lh-base fs-5">
     <dl class = "row">
         @foreach($auctionReports as $report)
-            <dt class = "col-sm-6 text-truncate">{{User::where('user_id', $report->user_id)->username}} said:</dt>
+            <dt class = "col-sm-6 text-truncate">{{$report->user_id}} in the auction {{$report->auction_id}}said:</dt>
             <dd class = "col-sm-9">
                 <p>{{$report->description}}</p>
             </dd>
