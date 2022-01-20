78789a7c2770df35eb768c654d5b5eec79851a67
up201907284 <up201907284@fe.up.pt>
2022-01-20 14:59:32 +0000

    syntax 2

--- resources/views/partials/auctionComplaints.blade.php
+++ resources/views/partials/auctionComplaints.blade.php
@@ -2,7 +2,7 @@
 
 <div class = "text-md-start lh-base fs-5">
     <dl class = "row">
-        @foreach($report in $auctionReports)
+        @foreach($auctionReports as $report)
             <dt class = "col-sm-6 text-truncate">{{$report->user_id}} said:</dt>
             <dd class = "col-sm-9">
                 <p>{{$report->description}}</p>
