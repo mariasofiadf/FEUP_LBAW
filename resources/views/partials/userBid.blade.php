928dc63250d286d2ae006716de2f60086a837a18
ritaramada <anaritaramada@gmail.com>
2022-01-21 18:04:22 +0000

    my bids

--- resources/views/partials/userBid.blade.php
+++ resources/views/partials/userBid.blade.php
@@ -1,17 +1,19 @@
 
-<div class = "container">
-  
-  <div class="row">
-  <div class="card sm m-1" style="margin-right: 1rem;">
-  <div class="card-body">
-      <div class= "row">
-        <h2 class="card-text">{{ $bid->auction()->first()->title }}</a></h2>
-        <span class="text-uppercase text-muted brand">{{ $bid->bid_value }}€</span><p></p>
-        <span class="text-uppercase text-muted brand">{{ $bid->bid_date }}</span>
-      </div>
-      <a href="/auctions/{{ $bid->auction_id }}" class="btn btn-outline-primary">Go to auction</a>
 
+
+<div class="container border p-1 mb-2 w-75">
+  <div class="row text-center align-middle">
+    <div class="col-sm align-self-center">
+      <h5 class="card-text">{{ $bid->auction()->first()->title }}</a></h5>
+    </div>
+    <div class="col-sm align-self-center">
+    <span class="text-uppercase text-muted brand">{{ $bid->bid_value }}€</span>
+    </div>
+    <div class="col-sm align-self-center">
+    <span class="text-uppercase text-muted brand">{{ $bid->bid_date }}</span>
+    </div>
+    <div class="col-sm text-end align-self-center">
+    <a href="/auctions/{{ $bid->auction_id }}" class="btn btn-outline-primary">Go to auction</a>
     </div>
   </div>
-</div>
 </div>
